2f984de865c119eed6bc2fdb1306afdfc722beb1
Mike Shultz <shultzm@gmail.com>
2017-07-09 21:38:36 -0600

    New error state test cases for managing managers

--- test/etherepmigration.errors.js
+++ test/etherepmigration.errors.js
@@ -87,6 +87,73 @@ contract("EtherepMigration", function(accounts) {
 
     });
 
+    it("should not not allow a manager to remove an admin", function() {
+
+        var mig;
+
+        return EtherepMigration.deployed().then(function(instance) {
+
+            mig = instance;
+
+            return mig.addManager(manager, {from: admin});
+
+        })
+        .then(function() {
+
+            return mig.removeAdmin(admin, {from: manager});
+
+        })
+        .then(assert.fail)
+        .catch(function(err) {
+            assert(err.message.indexOf('invalid opcode') >= 0, "should have thrown invalid opcode");
+        });
+
+    });
+
+    it("should not not allow a manager to add a manager", function() {
+
+        var mig;
+
+        return EtherepMigration.deployed().then(function(instance) {
+
+            mig = instance;
+
+            return mig.addManager(whoever, {from: manager});
+
+        })
+        .then(assert.fail)
+        .catch(function(err) {
+            assert(err.message.indexOf('invalid opcode') >= 0, "should have thrown invalid opcode");
+        });
+
+    });
+
+    it("should not not allow a manager to remove a manager", function() {
+
+        var mig;
+
+        return EtherepMigration.deployed().then(function(instance) {
+
+            mig = instance;
+
+            return mig.addManager(whoever, {from: admin});
+
+        })
+        .then(function() {
+
+            return mig.removeManager(whoever, {from: manager});
+
+        })
+        .then(assert.fail)
+        .catch(function(err) {
+            assert(err.message.indexOf('invalid opcode') >= 0, "should have thrown invalid opcode");
+            
+            // Cleanup
+            return mig.removeManager(whoever, {from: admin});
+        });
+
+    });
+
     it("should not not allow an admin to remove themself", function() {
 
         var mig;
