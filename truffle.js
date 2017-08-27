6d13ae63a54a63cebe7783d9c5e10915947ce064
Mike Shultz <shultzm@gmail.com>
2017-08-27 15:02:03 -0600

    Added coverage to truffle conf

--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 7545,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
     }
   }
 };
