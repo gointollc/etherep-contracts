0d5ed1a13eeb156d9447355be0428a4d8400f3a6
Mike Shultz <shultzm@gmail.com>
2017-08-27 15:00:50 -0600

    Removed migration contract from deploy and fixed a dependency issues

--- migrations/2_deploy_etherep.js
+++ migrations/2_deploy_etherep.js
@@ -1,6 +1,5 @@
 var RatingStore = artifacts.require("RatingStore");
 var Etherep = artifacts.require("Etherep");
-var EtherepMigration = artifacts.require("EtherepMigration");
 
 module.exports = function(deployer, network, accounts) {
   var manager = accounts[0];
@@ -10,24 +9,12 @@ module.exports = function(deployer, network, accounts) {
   deployer.deploy(RatingStore, manager, manager)
   // Deploy etherep
   .then(function() {
-    return deployer.deploy(Etherep, manager, fee, RatingStore.address, wait);
-  })
-  // set controller for RatingStore
-  .then(function() {
-    var rs = RatingStore.at(RatingStore.address);
-    return rs.setController(Etherep.address, {from: manager});
-  })
-  // Deploy migration contract
-  .then(function() {
-    var migrate;
-    return deployer.deploy(EtherepMigration, manager).then(function(res) {
-        migrate = EtherepMigration.at(EtherepMigration.address);
-        return migrate;
-    }).then(function(x) {
-        return migrate.setContract('etherep', Etherep.address, {from: manager});
-    }).then(function(x) {
-        return migrate.setContract('ratingstore', RatingStore.address, {from: manager});
-    });
+    return deployer.deploy(Etherep, manager, fee, RatingStore.address, wait)
+    // set controller for RatingStore
+    .then(function() {
+      var rs = RatingStore.at(RatingStore.address);
+      return rs.setController(Etherep.address, {from: manager});
+    })
   });
   
 };
